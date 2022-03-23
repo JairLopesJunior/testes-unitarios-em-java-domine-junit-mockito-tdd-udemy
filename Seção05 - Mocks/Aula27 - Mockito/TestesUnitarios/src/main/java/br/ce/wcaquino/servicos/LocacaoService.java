cfa959fe55ccc395bbe42a63b1ecd1487cb02778
unknown <jair.lopes@fatec.sp.gov.br>
2022-03-22 22:16:04 -0300

    Adicionado a Aula27 - Mockito

---
+++ Seção05 - Mocks/Aula27 - Mockito/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -0,0 +1,95 @@
+package br.ce.wcaquino.servicos;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.ce.wcaquino.daos.LocacaoDAO;
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
+import br.ce.wcaquino.exceptions.LocadoraException;
+import br.ce.wcaquino.utils.DataUtils;
+import buildermaster.BuilderMaster;
+
+public class LocacaoService {
+	
+	private LocacaoDAO dao;
+	
+	public Locacao alugarFilme(Usuario usuario, List<Filme> filmes) throws FilmeSemEstoqueException, LocadoraException {
+		if(usuario == null) {
+			throw new LocadoraException("Usuario vazio");
+		}
+		
+		if(filmes == null || filmes.isEmpty()) {
+			throw new LocadoraException("Filme vazio");
+		}
+
+		List<Filme> filterFilmes = filmes
+			.stream()
+			.filter(filme -> filme.getEstoque() == 0)
+			.collect(Collectors.toList());
+		
+		if(filterFilmes.size() > 0) {
+			throw new FilmeSemEstoqueException();
+		}
+		
+		Locacao locacao = new Locacao();
+		locacao.setFilmes(filmes);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		Double valorTotal = 0d;
+		for(int i = 0; i < filmes.size(); i++) {
+			Filme f = filmes.get(i);
+			Double valorFilme = f.getPrecoLocacao();
+			switch(i) {
+				case 2: valorFilme = valorFilme * 0.75; break;
+				case 3: valorFilme = valorFilme * 0.50; break;
+				case 4: valorFilme = valorFilme * 0.25; break;
+				case 5: valorFilme = 0d; break;
+			}
+			valorTotal += valorFilme;
+		}
+		locacao.setValor(valorTotal);		
+
+		//Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		if(DataUtils.verificarDiaSemana(dataEntrega, Calendar.SUNDAY)) {
+			dataEntrega = adicionarDias(dataEntrega, 1);
+		}
+		locacao.setDataLocacao(dataEntrega);
+		
+		// Salvando a locação...
+		dao.salvar(locacao);
+		
+		return locacao;
+	}
+	
+	public void setLocacaoDAO(LocacaoDAO dao) {
+		this.dao = dao;
+	}
+	
+	public static void main(String[] args) {
+		new BuilderMaster().gerarCodigoClasse(Locacao.class);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
