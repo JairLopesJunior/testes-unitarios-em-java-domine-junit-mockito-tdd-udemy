b6d91d6d6bd1d6e78517864d2beca66a04a2a6ed
unknown <jair.lopes@fatec.sp.gov.br>
2022-03-26 00:27:08 -0300

    Adicionado a Aula33 - Capturando Argumentos

---
+++ Seção05 - Mocks/Aula33 - Capturando Argumentos/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -0,0 +1,21 @@
+package br.ce.wcaquino.servicos;
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import br.ce.wcaquino.entidades.Locacao;
+
+public class CalculadoraMockTest {
+
+	@Test
+	public void teste() {
+		Calculadora calc = Mockito.mock(Calculadora.class);
+		
+		ArgumentCaptor<Integer> argCapt = ArgumentCaptor.forClass(Integer.class);
+		Mockito.when(calc.somar(argCapt.capture(), argCapt.capture())).thenReturn(5);
+		
+		System.out.println(calc.somar(1, 2));
+		System.out.println(argCapt.getAllValues());
+	}
+}
