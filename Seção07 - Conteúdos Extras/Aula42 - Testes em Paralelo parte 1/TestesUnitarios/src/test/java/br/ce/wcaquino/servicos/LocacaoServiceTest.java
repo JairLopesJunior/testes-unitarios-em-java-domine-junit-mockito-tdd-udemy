471e0fa87b80021a6db9e66e03616f5457a6b5d0
unknown <jair.lopes@fatec.sp.gov.br>
2022-03-27 21:57:42 -0300

    Adicionado a Aula42 - Testes em Paralelo parte 1

---
+++ Seção07 - Conteúdos Extras/Aula42 - Testes em Paralelo parte 1/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -0,0 +1,155 @@
+package br.ce.wcaquino.servicos;
+
+import static br.ce.wcaquino.builders.FilmeBuilder.umFilme;
+import static br.ce.wcaquino.builders.UsuarioBuilder.umUsuario;
+import static br.ce.wcaquino.matchers.MatchersProprios.caiNumaSegunda;
+import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.powermock.api.mockito.PowerMockito.doReturn;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import br.ce.wcaquino.daos.LocacaoDAO;
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.runners.ParallelRunner;
+import br.ce.wcaquino.utils.DataUtils;
+
+@RunWith(ParallelRunner.class)
+public class LocacaoServiceTest {
+
+	@InjectMocks 
+	@Spy
+	private LocacaoService service;
+	
+	@Mock
+	private SPCService spc;
+	@Mock
+	private LocacaoDAO dao;
+	@Mock
+	private EmailService emailService;
+	
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
+	@Before
+	public void setup(){
+		MockitoAnnotations.initMocks(this);
+		System.out.println("Inicializando2...");
+	}
+	
+	@After
+	public void tearDown() {
+		System.out.println("Finalizando...");
+	}
+	
+	@Test
+	public void deveAlugarFilme() throws Exception {
+		//cenario
+		Usuario usuario = umUsuario().agora();
+		List<Filme> filmes = Arrays.asList(umFilme().comValor(5.0).agora());
+		
+		doReturn(DataUtils.obterData(28, 4, 2017)).when(service).obterData();
+		
+		//acao
+		Locacao locacao = service.alugarFilme(usuario, filmes);
+			
+		//verificacao
+		error.checkThat(locacao.getValor(), is(equalTo(5.0)));
+		error.checkThat(isMesmaData(locacao.getDataLocacao(), DataUtils.obterData(28, 4, 2017)), is(true));
+		error.checkThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterData(29, 4, 2017)), is(true));
+	}
+	
+	@Test
+	public void deveDevolverNaSegundaAoAlugarNoSabado() throws Exception{		
+		//cenario
+		Usuario usuario = umUsuario().agora();
+		List<Filme> filmes = Arrays.asList(umFilme().agora());
+		
+		doReturn(DataUtils.obterData(29, 4, 2017)).when(service).obterData();
+		
+		//acao
+		Locacao retorno = service.alugarFilme(usuario, filmes);
+		
+		//verificacao
+		assertThat(retorno.getDataRetorno(), caiNumaSegunda());		
+	}
+	
+	@Test
+	public void deveCalcularValorLocacao() throws Exception {
+		// cenario
+		List<Filme> filmes = Arrays.asList(umFilme().agora());
+		
+		// ação
+		Class<LocacaoService> clazz = LocacaoService.class;
+		Method metodo = clazz.getDeclaredMethod("calcularValorLocacao", List.class);
+		metodo.setAccessible(true);
+		Double valor = (Double) metodo.invoke(service, filmes);
+		
+		// verificação
+		assertThat(valor, is(4.0));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
